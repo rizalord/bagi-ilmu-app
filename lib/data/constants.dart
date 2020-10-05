70bee74a48e2d3eed7c9bab1a6e605bdf414a732
rizalord <ahmadkhamdani9@gmail.com>
2020-10-05 15:15:42 +0700

    change host api

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 mixin URLs {
-  // static const String host = 'https://unord-rest-api.herokuapp.com/';
-  static const String host = 'http://192.168.43.233:1337/';
+  static const String host = 'https://unord-rest-api.herokuapp.com/';
+  // static const String host = 'http://192.168.43.233:1337/';
 }
 
 mixin AppLimit {
